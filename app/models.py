92d7b53d18c5b41af43498b208823c078c991d5e
Guilherme Dias <guilhermedias2309@gmail.com>
2026-06-03 11:08:06 -0300

    feat: add User model with email and hashed_password fields

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, DateTime
+from datetime import datetime
 from app.database import Base
 
 class Task(Base):
@@ -8,3 +9,12 @@ class Task(Base):
     titulo = Column(String, nullable = False)
     descricao = Column(String, nullable = True)
     concluida = Column(Boolean, default = False)
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key= True, index=True)
+    email = Column(String, unique= True)
+    hashed_password = Column(String, nullable=False)
+    create_at = Column(DateTime, default=datetime.utcnow)
