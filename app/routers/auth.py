6ff7286223187e261c9b6eb345c3a716b94696d5
Guilherme Dias <guilhermedias2309@gmail.com>
2026-06-03 14:04:30 -0300

    feat: add login route with JWT token generation

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,9 +1,9 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.database import SessionLocal
 from app.models import User
 from app.schemas import UserCreate, UserResponse
-from app.security import hash_password
+from app.security import hash_password, verify_password, create_access_token
 
 router = APIRouter()
 
@@ -23,3 +23,13 @@ def register(user: UserCreate, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_user)
     return new_user
+
+@router.post("/auth/login")
+def login(user: UserCreate, db: Session = Depends(get_db)):
+    db_user = db.query(User).filter(User.email == user.email).first()
+    if not db_user:
+        raise HTTPException(status_code=401, detail="Email ou senha inválidos")
+    if not verify_password(user.password, db_user.hashed_password):
+        raise HTTPException(status_code=401, detail="Email ou senha inválidos")
+    token = create_access_token(data={"sub": db_user.email})
+    return {"access_token": token, "token_type": "bearer"}
--- app/security.py
+++ app/security.py
@@ -1,7 +1,20 @@
 import bcrypt
+from jose import jwt
+from datetime import timedelta, datetime
+import os
+
+SECRET_KEY = os.getenv("SECRET_KEY", "minflow-secret-dev")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+def create_access_token(data:dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes= ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
 def hash_password(password: str) -> str:
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()).decode("utf-8")
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return bcrypt.checkpw(plain_password.encode("utf-8", hashed_password.encode("utf-8")))
+    return bcrypt.checkpw(plain_password.encode("utf-8"), hashed_password.encode("utf-8"))
